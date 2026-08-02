290729dda4ff9b5d32dea3690dff769705ce0d03
Debasis Panda <167549811+guddu-debasis@users.noreply.github.com>
2026-08-02 11:22:22 +0530

    Add examples of string immutability in JavaScript

--- String/2.Immutability_What Happens in Memory.js
+++ String/2.Immutability_What Happens in Memory.js
@@ -6,3 +6,25 @@ message = message + " World"; // Creates a brand new string "Hello World" in mem
 let score = "100";
 score[0] = "5";               // Silently fails (or throws an error in strict mode)
 console.log(score);           // Output: "100" (Unchanged)
+
+
+
+
+//
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
