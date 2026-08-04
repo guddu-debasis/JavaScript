3243991d607485803ec408e7fac55b7badb256e9
guddu-debasis <167549811+guddu-debasis@users.noreply.github.com>
2026-08-04 16:34:52 +0530

    add objects

---
+++ objects/1. Basic Syntax & Creation.js
@@ -0,0 +1,31 @@
+const user = {
+  name: "Alex",
+  age: 28,
+  isAdmin: true,
+  greet: function() {
+    console.log("Hello!");
+  }
+};
+
+
+console.log(user.name);       // "Alex" (Dot notation)
+console.log(user["age"]);     // 28 (Bracket notation)
+
+
+user.age = 29;                // Updates existing property
+user.country = "Canada";      // Adds a new property
+
+
+delete user.isAdmin;          // Removes the isAdmin key
+
+
+/*
+Object.keys(obj): Returns an array of an object's keys.
+Object.values(obj): Returns an array of an object's values.
+Object.entries(obj): Returns an array of [key, value] pairs.
+Object.assign(target, source): Merges properties from one object into another.
+*/
+
+for (let key in user) {
+  console.log(`${key}: ${user[key]}`);  // iterates over each key-value pair in the user object
+}
