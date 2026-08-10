2c03458de11d480483a3e16108841ebcbbfea8a4
Debasis Panda <167549811+guddu-debasis@users.noreply.github.com>
2026-08-10 12:22:47 +0530

    Add color change functionality on button click

--- DOM/DOM1/script.js
+++ DOM/DOM1/script.js
@@ -1,1 +1,12 @@
+const s=document.querySelectorAll('.button')
+const div=document.querySelector('.canvas')
+const h2=document.createElement('h2')
+div.append(h2)
+
+s.forEach((s)=>{
+  s.addEventListener('click',(event)=>{
+    document.body.style.backgroundColor=`${s.id}`
+    h2.innerText=`The color of the backgound is:${s.id}`
+  })
+})
 
