f070b169677a0b1b1d182499d163f5973494d55c
guddu-debasis <167549811+guddu-debasis@users.noreply.github.com>
2026-08-10 19:37:00 +0530

    change s to btn

--- DOM/DOM1/script.js
+++ DOM/DOM1/script.js
@@ -1,9 +1,9 @@
-const s=document.querySelectorAll('.button')
+const btn=document.querySelectorAll('.button')
 const div=document.querySelector('.canvas')
 const h2=document.createElement('h2')
 div.append(h2)
 
-s.forEach((s)=>{
+btn.forEach((s)=>{
   s.addEventListener('click',(event)=>{
     document.body.style.backgroundColor=`${s.id}`
     h2.innerText=`The color of the backgound is:${s.id}`
